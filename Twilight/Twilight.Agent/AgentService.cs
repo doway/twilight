64c180b970915013302b655eb6ee2906e03d77f2
Tom Tang <tom@1stlight.us>
2013-10-31 18:52:02 +0800

    Agent project

---
+++ Twilight/Twilight.Agent/AgentService.cs
@@ -0,0 +1,56 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Reflection;
+using System.ServiceModel.Activities;
+using System.ServiceModel.Description;
+using System.ServiceProcess;
+using System.Xaml;
+
+namespace Twilight.Agent
+{
+    public partial class AgentService : ServiceBase
+    {
+        private static readonly ILog _logger = LogManager.GetLogger(typeof(AgentService));
+        private WorkflowServiceHost host = null;
+        public AgentService()
+        {
+            InitializeComponent();
+        }
+
+        public void Start(string[] args)
+        {
+            OnStart(args);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string log4netCfgPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"\log4net.config";
+            XmlConfigurator.ConfigureAndWatch(new FileInfo(log4netCfgPath));
+
+            string serviceXmalPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"\DeployService.xamlx";
+            WorkflowServiceHost host = new WorkflowServiceHost(XamlServices.Load(serviceXmalPath), new Uri("http://localhost:2209/Services/DeployService"));
+            host.Faulted += host_Faulted;
+            host.UnknownMessageReceived += host_UnknownMessageReceived;
+            host.AddDefaultEndpoints();
+            host.Description.Behaviors.Add(new ServiceMetadataBehavior { HttpGetEnabled = true });
+            host.Open();
+        }
+
+        void host_UnknownMessageReceived(object sender, System.ServiceModel.UnknownMessageReceivedEventArgs e)
+        {
+            _logger.Warn(e.Message);
+        }
+
+        void host_Faulted(object sender, EventArgs e)
+        {
+            _logger.Fatal(e);
+        }
+
+        protected override void OnStop()
+        {
+            if (null != host) host.Close();
+        }
+    }
+}
---
+++ Twilight/Twilight.Agent/Program.cs
@@ -0,0 +1,38 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twilight.Agent
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            GlobalContext.Properties["appname"] = "TwilightAgent";
+            if (args.Length > 0 && (args[0].ToLower() == "-c" || args[0].ToLower() == "/c"))
+            {
+                var service = new AgentService();
+                service.Start(args);
+                Console.WriteLine("The service[{0}] has been activated.\r\nPlease press [ENTER] to stop it.", GlobalContext.Properties["appname"]);
+                Console.ReadLine();
+                service.Stop();
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[] 
+            { 
+                new AgentService() 
+            };
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+    }
+}
